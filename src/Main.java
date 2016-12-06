cd56e86f56701d6a6bf8313d01286bfb7ea7bf8b
spasynkov <s.pasynkov@gmail.com>
2016-12-06 18:08:34 +0200

    Main class updated

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,17 @@
-import indexing.ISearcherImpl;
+import indexing.ISearcher;
+import indexing.ISearcherImplTree;
 
 public class Main {
     public static void main(String[] args) {
         String[] names = {"aaa", "aaa", "aab", "aab", "aac", "abc", "acb", "aaa", "aaa"};
         long[] dates = {1, 3, 2, 1, 3, 2, 1, 3, 2};
+        String guess = "aa";
 
-        ISearcherImpl searcher = new ISearcherImpl();
+        ISearcher searcher = new ISearcherImplTree();
         searcher.refresh(names, dates);
-        searcher.print();
+
+        for (String s : searcher.guess(guess)) {
+            System.out.println(s);
+        }
     }
 }
